28202b64476616a4802af866fdc3820e401bdd02
johnata soares <johnata.soares@enacom.com.br>
2023-03-01 15:46:20 -0300

    changed tag

--- Examples/js/dp_inv.js
+++ Examples/js/dp_inv.js
@@ -1,4 +1,4 @@
-import * as UAIbot from 'https://cdn.jsdelivr.net/gh/UAIbot/UAIbotJS@main/UAIbotJS/UAIbot.js';
+import * as UAIbot from 'https://cdn.jsdelivr.net/gh/UAIbot/UAIbotJS@v1.0.1/UAIbotJS/UAIbot.js';
 import * as Utils from 'https://cdn.jsdelivr.net/gh/UAIbot/UAIbotJS@main/UAIbotJS/Utils.js';
 import * as math from 'https://cdn.jsdelivr.net/npm/mathjs@11.6.0/+esm'
 
